c606ada487d95c777df8d5bb27886dc321ceabdc
zyh <395060139@qq.com>
2020-11-18 19:02:09 +0800

    手写了一遍

--- app/src/main/java/com/zyh/algorithmsstudy/sort/Sort.java
+++ app/src/main/java/com/zyh/algorithmsstudy/sort/Sort.java
@@ -2,13 +2,13 @@ package com.zyh.algorithmsstudy.sort;
 
 public class Sort {
     public static void main(String[] args) {
-        int[] a = new int[]{3,5,6,4,1,7};
-        selectionSort(a);
+        int[] a = new int[]{3,5,6,4,1,7,0};
+        insertSort(a);
     }
 
     private static void bubbleSort(int[] a) {
         for (int i = 0; i < a.length; i++) {
-            for (int j = 1; j < a.length; j++) {
+            for (int j = 1; j < a.length - i; j++) {
                 if (a[j - 1] > a[j]) {
                     int temp = a[j - 1];
                     a[j - 1] = a[j];
@@ -21,9 +21,9 @@ public class Sort {
         }
     }
 
-    private static void insertSort(int[] a) {
+    private static void insertSort(int[] a) {   //{3,5,6,4,1,7,0}
         for (int i = 1; i < a.length; i++) {
-            int temp = a[i];        //每次拿一个数
+            int temp = a[i];
             int j = i - 1;
             for(; j >= 0; j--) {    //与temp前面的已排序数组比较
                 if (a[j] > temp) {
@@ -40,7 +40,7 @@ public class Sort {
     }
 
     private static void selectionSort(int[] a) {
-        for (int i = 0; i < a.length; i++) {
+        for (int i = 0; i < a.length - 1; i++) {
             for (int j = i + 1; j < a.length; j++) {
                 if (a[i] > a[j]) {
                     int temp = a[i];
