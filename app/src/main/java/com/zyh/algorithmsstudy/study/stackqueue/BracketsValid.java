469b70474e7c1a7d7255799f3ac974f3893d9780
zyh <395060139@qq.com>
2021-02-20 15:07:02 +0800

    栈的题

--- app/src/main/java/com/zyh/algorithmsstudy/study/stackqueue/BracketsValid.java
+++ app/src/main/java/com/zyh/algorithmsstudy/study/stackqueue/BracketsValid.java
@@ -30,4 +30,26 @@ public class BracketsValid {
         }
         return stack.isEmpty();
     }
+
+    public boolean isValidTest(String s) {
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < s.length(); i++) {
+            switch (s.charAt(i)) {
+                case '(' :
+                    stack.push(')');
+                    break;
+                case '[' :
+                    stack.push(']');
+                    break;
+                case '{' :
+                    stack.push('}');
+                    break;
+                default:
+                    if (stack.isEmpty() || s.charAt(i) != stack.pop()) {
+                        return false;
+                    }
+            }
+        }
+        return stack.isEmpty();
+    }
 }
