64b8831b8edf0f7b42057a57ef273b617d41e170
zyh <395060139@qq.com>
2021-04-06 19:02:48 +0800

    4没写完

---
+++ app/src/main/java/com/zyh/algorithmsstudy/leetCode/LeetCode3.java
@@ -0,0 +1,31 @@
+package com.zyh.algorithmsstudy.leetCode;
+
+import java.util.LinkedList;
+
+/**
+ * 给定一个字符串，请你找出其中不含有重复字符的 最长子串 的长度
+ */
+public class LeetCode3 {
+    public static void main(String[] args) {
+        //abcabcbb
+        int num = lengthOfLongestSubstring("abcabcbb");
+        System.out.print(num);
+    }
+
+    public static int lengthOfLongestSubstring(String s) {
+        if (s.length() <= 1) return s.length();
+
+        LinkedList<Character> queue = new LinkedList<>();
+
+        int result = 0;
+        for (int i = 0; i < s.length(); i++) {
+            while (queue.contains(s.charAt(i))) {
+                result = Math.max(result, queue.size());
+                queue.poll();
+            }
+            queue.offer(s.charAt(i));
+        }
+
+        return Math.max(result, queue.size());
+    }
+}
---
+++ app/src/main/java/com/zyh/algorithmsstudy/leetCode/LeetCode4.java
@@ -0,0 +1,34 @@
+package com.zyh.algorithmsstudy.leetCode;
+
+public class LeetCode4 {
+    public static void main(String[] args) {
+        double v = findMedianSortedArrays(new int[]{1, 3}, new int[]{2});
+        System.out.print(v);
+    }
+
+    public static double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        int index1 = 0;
+        int index2 = 0;
+        int temp = 0;
+
+        int[] tempArray = new int[nums1.length + nums2.length];
+
+        while(index1 < nums1.length || index2 < nums2.length) {
+            if (nums1[index1] <= nums2[index2]) {
+                tempArray[temp++] = nums1[index1++];
+            }else {
+                tempArray[temp++] = nums2[index2++];
+            }
+        }
+        for (int i = 0; i < tempArray.length; i++) {
+            System.out.println(tempArray[i]);
+        }
+
+        int index = tempArray.length / 2;
+        if (tempArray.length % 2 == 0) {
+            return ((double) tempArray[index] + (double)tempArray[index - 1]) / 2;
+        }else {
+            return (double) tempArray[index];
+        }
+    }
+}
